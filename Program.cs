026c4c22768595e18b373f59d487ead4840d181f
Abhishek shukla <Abhishekgargshu786@gmail.com>
2023-03-24 18:32:47 +0530

    The programme file

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Addressbook
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+          //  UC_1 obj = new UC_1();
+            //obj.solution();
+            Welcome obj
+                = new Welcome();
+            obj.soluton();
+        }
+    }
+}
