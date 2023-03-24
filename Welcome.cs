8f85479bdf872dc681081ca87dbe19fc455de9fb
Abhishek shukla <Abhishekgargshu786@gmail.com>
2023-03-24 18:31:17 +0530

    Welcome

---
+++ Welcome.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Addressbook
+{
+    public class Welcome
+    {
+        public void soluton()
+        {
+            Console.WriteLine("Welcome to Addressbook");
+            Console.ReadLine();
+        }
+    }
+}
